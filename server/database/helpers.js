2117ddde70777c88305b9b5008aa7f7b0d7add1b
Emma Li <emma.m.li.98@gmail.com>
2020-09-24 17:40:31 -0700

    Creates database helper functions
    Return item reviews, store reviews, and photo reviews of the item first, then from the rest of the store.
    I also created an extra helper function to grab the storeId if given an itemId to aid with the latter two
    functions.

---
+++ server/database/helpers.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const Review = require('./Review.js');
+mongoose.Promise = global.Promise;
+
+const getItem = function(itemId) {
+  return Review.find({ itemId }).sort({ createdAt: -1 });
+};
+
+const getStoreId = function(itemId) {
+  return Review.find({ itemId })
+    .then((docs) => {
+      return docs[0].storeId;
+    })
+}
+
+const getStore = function(storeId) {
+  return Review.find({ storeId }).sort({ createdAt: -1 });
+}
+
+const getPhoto = function(itemId, storeId) {
+  return Promise.all([
+    Review.find({ itemId, imageUrl: { $ne: null } }).sort({ createdAt: -1 }),
+    Review.find({ storeId, imageUrl: { $ne: null } }).sort({ createdAt: -1 }),
+  ])
+    .then((results) => {
+      return results.flat();
+    });
+}
+
+module.exports = {
+  getItem,
+  getStore,
+  getPhoto,
+  getStoreId
+}
