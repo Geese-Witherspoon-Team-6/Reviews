9c415fc95ae0c2327ffe8ad96d06eb3fb85ed4c3
Emma Li <emma.m.li.98@gmail.com>
2020-09-24 17:55:05 -0700

    Sets up item-reviews api route on server

--- server/app.js
+++ server/app.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const path = require('path')
+const reviews = require('./database/helpers.js')
 const app = express()
 const port = 3001
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, '../client/dist')))
+
+app.get('/api/item-reviews/:itemId', (req, res) => {
+  reviews.getItem(req.params.itemId)
+    .then((reviews) => {
+      res.status(200).send(reviews);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    })
+})
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
