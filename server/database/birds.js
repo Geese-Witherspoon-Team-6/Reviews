3586989d719051f8a7100deb76d7a8e3e287391f
Emma Li <emma.m.li.98@gmail.com>
2020-10-02 12:12:52 -0700

    Edits seed data to use Etsy-sourced images, vary helpful field values

---
+++ server/database/birds.js
@@ -0,0 +1,31 @@
+const birds = [
+  'https://qtsy-cropped.s3-us-west-2.amazonaws.com/7022hub_web_large.jpg',
+  'https://qtsy-cropped.s3-us-west-2.amazonaws.com/7024loo-web_large.jpg',
+  'https://qtsy-cropped.s3-us-west-2.amazonaws.com/7027dev_web_large.jpg',
+  'https://qtsy-cropped.s3-us-west-2.amazonaws.com/7029she_web_large.jpg',
+  'https://qtsy-cropped.s3-us-west-2.amazonaws.com/7033liq_web_large.jpg',
+  'https://qtsy-cropped.s3-us-west-2.amazonaws.com/7034pod_web_large.jpg',
+  'https://qtsy-cropped.s3-us-west-2.amazonaws.com/I_poop_on_fascists_magnet_large.jpg',
+  'https://qtsy-cropped.s3-us-west-2.amazonaws.com/another_drink_wouldnt_hurt_magnet_large.jpg',
+  'https://qtsy-cropped.s3-us-west-2.amazonaws.com/anxiety_and_caffeine_magnet_large.jpg',
+  'https://qtsy-cropped.s3-us-west-2.amazonaws.com/artist-web_large.jpg',
+  'https://qtsy-cropped.s3-us-west-2.amazonaws.com/corn-chip-web_large.jpg',
+  'https://qtsy-cropped.s3-us-west-2.amazonaws.com/crazy-wide-web_large.jpg',
+  'https://qtsy-cropped.s3-us-west-2.amazonaws.com/da_fuq__Troubled_Bird_magnet_large.jpg',
+  'https://qtsy-cropped.s3-us-west-2.amazonaws.com/dealing-with-you-web_large.jpg',
+  'https://qtsy-cropped.s3-us-west-2.amazonaws.com/dont_judge_Troubled_Bird_magnet_large.jpg',
+  'https://qtsy-cropped.s3-us-west-2.amazonaws.com/drink-web_large.jpg',
+  'https://qtsy-cropped.s3-us-west-2.amazonaws.com/eff_all_yall_7081eff_large.jpg',
+  'https://qtsy-cropped.s3-us-west-2.amazonaws.com/evolve__Troubled_Bird_magnet_large.jpg',
+  'https://qtsy-cropped.s3-us-west-2.amazonaws.com/fascist_fuck_7084fuc_large.jpg',
+  'https://qtsy-cropped.s3-us-west-2.amazonaws.com/god-cant-help-web_large.jpg',
+  'https://qtsy-cropped.s3-us-west-2.amazonaws.com/heebie-jeebies-web_large.jpg',
+  'https://qtsy-cropped.s3-us-west-2.amazonaws.com/humanphobic_troubled_bird_magnet_large.jpg',
+  'https://qtsy-cropped.s3-us-west-2.amazonaws.com/oh_jesus_oh_god_magnet_large.jpg',
+  'https://qtsy-cropped.s3-us-west-2.amazonaws.com/risk-web_large.jpg',
+  'https://qtsy-cropped.s3-us-west-2.amazonaws.com/singing-web_large.jpg',
+  'https://qtsy-cropped.s3-us-west-2.amazonaws.com/woke-ass_citizen_magnet_grande_3c150ebc-5f0d-4ee0-a404-469d1e053399_large.jpg',
+  'https://qtsy-cropped.s3-us-west-2.amazonaws.com/whom-web_large.jpg'
+];
+
+module.exports = birds;
