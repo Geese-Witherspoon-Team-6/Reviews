4219eb94fe0cc9e68766dbe420427b8f54716424
Emma Li <emma.m.li.98@gmail.com>
2020-10-05 11:36:25 -0700

    Serves webpack bundle as static file for proxy purposes
    Note: this method works except that styling is not sent over to the proxy,
    meaning I am probably going to have to switch to styled components.

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use('/listing/:itemId', express.static(path.join(__dirname, '../client/dist')))
+app.use(express.static(path.join(__dirname, '../client/dist')))
 
 app.get('/api/item-reviews/:itemId/:sort', (req, res) => {
   reviews.getItem(req.params.itemId, req.params.sort)
