2eba1a6c4ae6842f0c86217844c334a06d5426cc
Emma Li <emma.m.li.98@gmail.com>
2020-10-06 16:10:58 -0700

    Changes request paths to be absolute, handles CORS requests

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const path = require('path')
 const reviews = require('./database/helpers.js')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use('/listing/:itemId', express.static(path.join(__dirname, '../client/dist')))
