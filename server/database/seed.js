34cbca8df5ce08c510b4d9e2e016ccfc937f42ab
Emma Li <emma.m.li.98@gmail.com>
2020-10-08 11:40:43 -0700

    Switches to using S3 hosted images to fix rendering bug

--- server/database/seed.js
+++ server/database/seed.js
@@ -1,21 +1,22 @@
 const faker = require('faker');
 const Review = require('./Review.js');
 const db = require('./index.js');
-const birds = require('./birds.js');
+const images = require('./birds.js');
 
 const mockData = [];
+let reviewImageIdx = 0;
 
 // Mock Data
 for (var i = 1; i <= 100; i++) {
   for (var j = 0; j < 5; j++) {
     var itemName = `${faker.commerce.productName()} - ${faker.commerce.productAdjective()}, ${faker.commerce.department()}`;
-    var itemThumb = birds[i % birds.length];
+    var itemThumb = images.birds[i % images.birds.length];
     var imageUrl = null;
 
     var hasPhoto = Boolean(Math.round(Math.random()));
     if (hasPhoto) {
-      // SDC Fix - all seed data has the same photo review image
-      imageUrl = 'https://i.imgur.com/bf6OaZl.jpg';
+      imageUrl = images.reviewImages[reviewImageIdx % images.reviewImages.length];
+      reviewImageIdx++;
     }
 
     var newReview = {
@@ -40,10 +41,7 @@ for (var i = 1; i <= 100; i++) {
 // Editing data for demo
 for (var i = 0; i < 5; i++) {
   mockData[i].itemName = 'The Risk I Took Card - Bird - Greeting - Math - Humor - Mincing Mockingbird';
-  mockData[i].itemThumb = 'https://i.imgur.com/V46YJKJ.jpg';
-  if (mockData[i].imageUrl) {
-    mockData[i].imageUrl = i % 2 ? 'https://i.imgur.com/dxcQPYQ.jpg' : 'https://i.imgur.com/21Qq1X9.jpg';
-  }
+  mockData[i].itemThumb = 'https://qtsy-reviews.s3.us-east-2.amazonaws.com/icon.jpg';
 }
 
 // Inserting seed data
